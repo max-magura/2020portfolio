78b11314749b111a5d0d6954f4825e5002ce6fd2
Max-M <maxnmagura@gmail.com>
2020-01-18 12:12:48 -0600

    build folder

---
+++ build/precache-manifest.8f01e520cf0beec06c69ca45dc8ef1be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e93473f09ea8a73435b3d45da0fab27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c2c275ecd2508fd7f20",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f534915aff978e5b65c5",
+    "url": "/static/js/2.162d23a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.162d23a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c2c275ecd2508fd7f20",
+    "url": "/static/js/main.b66d0cad.chunk.js"
+  },
+  {
+    "revision": "fa4987fcf3c69a0bc711",
+    "url": "/static/js/runtime-main.d2309a14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
